a99fc520ace7b35db676b5d3ac79e31caa305441
Pavel Trutman <pavel.trutman@fel.cvut.cz>
2016-06-15 16:57:27 +0200

    Added script for computing backup sizes.

---
+++ backupSize.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import os
+import shutil
+import time
+import math
+import sys
+import re
+import goldFish
+
+
+if __name__ == '__main__':
+  
+  dirFrom = str(sys.argv[1])
+
+  goldFish.printHeadline()
+  print('Folder to inspect:' + dirFrom)
+  sizeTotal = 0
+  sizeDelete = 0
+ 
+  for root, dirs, files in os.walk(dirFrom):
+    relPath = os.path.relpath(root, dirFrom)
+    for file in files:
+      fileFrom = os.path.join(root, file)
+      goldFish.printToTerminalSize(os.path.join(relPath, file))
+      sys.stdout.flush()
+
+      statFrom = os.stat(fileFrom)
+      if statFrom.st_nlink == 1:
+        sizeDelete += statFrom.st_size
+      sizeTotal += statFrom.st_size
+
+      sys.stdout.write('\r')
+      sys.stdout.flush()
+      goldFish.printToTerminalSize(' ')
+      sys.stdout.write('\r')
+      sys.stdout.flush()
+
+  print('  Size of the backup:           ' + goldFish.readableSize(sizeTotal))
+  print('  Will be freed after deletion: ' + goldFish.readableSize(sizeDelete))
+
